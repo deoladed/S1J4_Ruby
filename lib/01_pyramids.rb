6ddd767a731ea6f0d5ae1cd4683f25dfac2ff866
deoladed <leo.robert.mk@gmail.com>
2019-01-10 13:37:40 +0100

    Ajout pyramide

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,34 @@
+def asknumber
+	puts "Oh grand pharaon, combien d'etages a ta pyramide ?"
+	h = gets.chomp.to_i
+	return h
+end
+
+def full_pyramid(h)
+	h.times do |i|
+		print ' ' * (h - i)
+		puts "#" * (i * 2 + 1)
+	end
+end
+
+
+def full_pyramidreverse(h)
+
+	h.times do |i|
+		print ' ' * (i) 
+		puts "#" * ((h * 2 + 1) - (2 * i))
+
+		if ((h * 2 + 1) - (2 * i)) == 3
+		puts  ' ' * (i+1) + "#"
+		end
+	end
+end
+
+
+
+def wtf_pyramid(h)
+full_pyramid(h)
+full_pyramidreverse(h)
+end
+
+wtf_pyramid(asknumber)
